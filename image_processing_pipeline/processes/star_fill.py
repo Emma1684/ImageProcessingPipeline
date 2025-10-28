2a233c4184848cfea1faf26b64a9c1f01d919320
Kevin Kleinbeck <KeKleinbeck@gmail.com>
2025-10-28 17:32:55 +0100

    Added a collecting option to the serialiser, did some minor clean ups at the ProcessData Interfaces and fixed a bug at star_fill

--- image_processing_pipeline/processes/star_fill.py
+++ image_processing_pipeline/processes/star_fill.py
@@ -15,11 +15,11 @@ class StarFill(AbstractProcessStep):
     not checked however. Using this process for other inputs is considered
     undefined behaviour. 
     """
-    cs1 = np.cumsum(self.input_stack, axis=1)
+    cs1 = np.cumsum(self.input_mask, axis=1)
     inner1 = cs1[:,-1,:][:,None,:] * cs1 - cs1**2
-    cs2 = np.cumsum(self.input_stack, axis=2)
+    cs2 = np.cumsum(self.input_mask, axis=2)
     inner2 = cs2[:,:,-1][:,:,None] * cs2 - cs2**2
-    self.output_mask = inner1 * inner2 >= 1
+    self.output_mask = (inner1 * inner2 >= 1).astype(np.int32)
 
 
 process_steps["StarFill"] = StarFill
